1594a430f0f32f3756cfa3dbb51039e8e890c29b
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-09 20:33:48 +0545

    mongodb connection by mongoose to nodejs and using json data

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express")
 const app =  express()
+const mongoose = require('mongoose')
 
 /* Alternative of first two lines
   const app = require('express')()
 */
 
+const ConnectionString = "mongodb+srv://jagadish:passJB@cluster0.t5syfqt.mongodb.net/?retryWrites=true&w=majority"
+
+async function connectToDatabase(){
+  await  mongoose.connect(ConnectionString)
+  console.log("Connected To DB Successfully")
+}
+connectToDatabase()
+
+
+
 app.get("/",(req,res)=>{
   //console.log(req)
-  res.send("Hello World")
+  res.status(200).json({
+    "message" : "success"
+  })
 })
 
 app.listen(3000,()=>{
