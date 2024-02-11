87ed1f3d088ce17e18c0ed4c3d42cd0c7b5f7b5e
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-11 19:39:13 +0545

    Model and post, get, getbyid api added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express")
 const app =  express()
 
 const connectToDatabase = require("./Database")
+const Book = require("./Model/bookModel")
 
 /* Alternative of first two lines
   const app = require('express')()
 */
+
+app.use(express.json())
 
 connectToDatabase()
 
@@ -14,6 +17,43 @@ app.get("/",(req,res)=>{
     "message" : "success"
   })
 })
+
+app.post("/book",(req,res)=>{
+
+  const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication} = req.body
+  Book.create({
+    bookName,
+    bookPrice,
+    isbnNumber,
+    authorName,
+    publishedAt,
+    publication
+  })
+
+  res.status(201).json({
+    message : "Book Created Successfully"
+  })
+
+})
+
+app.get("/book", async(req,res)=>{
+  const books = await Book.find()    // returns array
+  res.status(200).json({
+    message : " Book Fetched Successfully",
+    data : books
+  })
+})
+
+app.get("/book/:id",async(req,res)=>{
+  const id = req.params.id
+  const  book = await Book.findById(id)    // returns object
+  res.status(200).json({
+    message : "Single Book Fetched Successfully",
+    data : book
+  })
+})
+
+
 
 app.listen(3000,()=>{
   console.log("Nodejs server has been started at port 3000")
