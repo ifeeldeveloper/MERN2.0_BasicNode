1564c50df1d9f99c608ddc80f484b5a4284d45f9
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-18 17:25:03 +0545

    Middleware(multer) added for file handling

--- Model/bookModel.js
+++ Model/bookModel.js
@@ -1,27 +1,27 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const bookSchema = new Schema({
-  bookName : {
-    type : String,
-    unique : true
+  bookName: {
+    type: String,
+    unique: true
   },
-  bookPrice : {
-    type : Number
+  bookPrice: {
+    type: Number
   },
-  isbnNumber : {
-    type : Number
+  isbnNumber: {
+    type: String
   },
-  authorName : {
-    type : String
+  authorName: {
+    type: String
   },
-  publishedAt : {
-    type : String
+  publishedAt: {
+    type: String
   },
-  publication : {
-    type : String
+  publication: {
+    type: String
   }
 })
 
-const Book = mongoose.model('Book',bookSchema)
+const Book = mongoose.model('Book', bookSchema)
 module.exports = Book
--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const express = require("express")
-const app =  express()
-
-const connectToDatabase = require("./Database")
-const Book = require("./Model/bookModel")
+const app = express()
 
 /* Alternative of first two lines
   const app = require('express')()
 */
+const connectToDatabase = require("./Database")
+const Book = require("./Model/bookModel")
 
+
+// multer config imports
+
+const { multer, storage } = require("./Middleware/multerConfig")
+const upload = multer({ storage: storage })
+
+/* Alternative of line 13 and 14
+const multer = require("./Middleware/multerConfig").multer
+const storage = require(".Middleware/multerConfig").storage
+*/
 app.use(express.json())
 
 connectToDatabase()
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.status(200).json({
-    "message" : "success"
+    "message": "success"
   })
 })
 
-app.post("/book",(req,res)=>{
+app.post("/book", upload.single("image"), (req, res) => {
 
-  const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication} = req.body
+  const { bookName, bookPrice, isbnNumber, authorName, publishedAt, publication } = req.body
   Book.create({
     bookName,
     bookPrice,
@@ -31,55 +40,55 @@ app.post("/book",(req,res)=>{
   })
 
   res.status(201).json({
-    message : "Book Created Successfully"
+    message: "Book Created Successfully"
   })
 
 })
 
-app.get("/book", async(req,res)=>{
+app.get("/book", async (req, res) => {
   const books = await Book.find()    // returns array
   res.status(200).json({
-    message : " Book Fetched Successfully",
-    data : books
+    message: " Book Fetched Successfully",
+    data: books
   })
 })
 
-app.get("/book/:id",async(req,res)=>{
+app.get("/book/:id", async (req, res) => {
   const id = req.params.id
-  const  book = await Book.findById(id)    // returns object
+  const book = await Book.findById(id)    // returns object
   res.status(200).json({
-    message : "Single Book Fetched Successfully",
-    data : book
+    message: "Single Book Fetched Successfully",
+    data: book
   })
 })
 
- 
-app.delete("/book/:id", async(req,res)=>{
+
+app.delete("/book/:id", async (req, res) => {
   const id = req.params.id
   await Book.findByIdAndDelete(id)
   res.status(200).json({
-    message : "Book Deleted Successfully"
+    message: "Book Deleted Successfully"
 
   })
 })
 
 
-app.patch("/book/:id",async(req,res)=>{
+app.patch("/book/:id", async (req, res) => {
   const id = req.params.id
-  const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication} = req.body
-  await Book.findByIdAndUpdate(id,{
-    bookName : bookName,
-    bookPrice : bookPrice,
-    isbnNumber : isbnNumber,
-    authorName : authorName,
-    publishedAt : publishedAt,
-    publication : publication
+  const { bookName, bookPrice, isbnNumber, authorName, publishedAt, publication } = req.body
+  await Book.findByIdAndUpdate(id, {
+    bookName: bookName,
+    bookPrice: bookPrice,
+    isbnNumber: isbnNumber,
+    authorName: authorName,
+    publishedAt: publishedAt,
+    publication: publication
   })
   res.status(200).json({
-    message : "Book Updated Successfully"
+    message: "Book Updated Successfully"
   })
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Nodejs server has been started at port 3000")
 })
